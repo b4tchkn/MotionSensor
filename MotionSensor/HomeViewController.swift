63933342925b6d547f194a8e09e81a1df1c9fdb9
Unknown <baaaakkrad@gmail.com>
2019-02-11 15:41:00 +0900

    [add]ファイル名用textField作成

--- MotionSensor/HomeViewController.swift
+++ MotionSensor/HomeViewController.swift
@@ -8,13 +8,26 @@
 
 import UIKit
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, UITextFieldDelegate {
 
+    
+    @IBOutlet weak var inputText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        inputText.delegate = self
     }
+    
     @IBAction func startButtonTapped(_ sender: Any) {
         performSegue(withIdentifier: "toSensorViewController", sender: nil)
+    }
+    
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        textField.resignFirstResponder()
+        return true
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
 }
